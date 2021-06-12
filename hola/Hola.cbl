c71ae990c18b14e03e6b311907b059e951e458a3
masterblck <198add@gmail.com>
2021-06-11 22:30:43 -0500

    Primer commit

---
+++ hola/Hola.cbl
@@ -0,0 +1,7 @@
+      *Programa de hola en COBOL
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLA.
+       PROCEDURE DIVISION.
+           DISPLAY "Hola este es mi primer programa en COBOL".
+           STOP RUN.
+        
